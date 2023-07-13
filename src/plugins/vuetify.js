d2fdbb43c7dd8526ae73ebb925eeb27abf1e617b
lesterivanrd <lesterivan.dalaguit@gmail.com>
2023-07-13 10:52:23 +0800

    dialog form and theme changes

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -12,9 +12,10 @@ export default new Vuetify({
   theme: {
     themes: {
       light: {
-        primary: '#1976D2',
-        secondary: '#424242',
-        accent: '#82B1FF',
+        primary: '#263238',
+        secondary: '#F5F5F5',
+        thirtiary: '#FFFFFF',
+        accent: '#3E2723',
         error: '#FF5252',
         info: '#2196F3',
         success: '#4CAF50',
